09d8880666a6c98ea45f00c3a5dcedbfba1bcf39
devgeetech <devgeetech@gmail.com>
2019-10-23 12:00:17 +0530

    change menu position to center

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,9 +8,9 @@ import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <DrawerToggle clicked = {props.drawerToggleClicked} />
-        <div className={classes.Logo}>
+        {/* <div className={classes.Logo}>
             <Logo />
-        </div>
+        </div> */}
         <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
