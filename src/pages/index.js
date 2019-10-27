8a5bc1ec2a185bdf473c3917a3e625375796bc09
devgeetech <devgeetech@gmail.com>
2019-10-27 16:11:37 +0530

    fix mob video 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,47 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 
 import Layout from '../components/hoc/Layout/Layout'
 
 import classes from '../styles/index.module.css'
-import HeroVideo from '../../content/vids/rasGli.mp4'
+import HeroPCVid from '../../content/vids/rasGli.mp4'
 import HeroMobVid from '../../content/vids/rasGliMob.mp4'
 
 
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
+  
+function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(
+      getWindowDimensions()
+    );
+  
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+  
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+  
+    return windowDimensions;
+}
+
 const IndexPage = () => {
+    const { height, width } = useWindowDimensions();
+    let HeroVid = HeroPCVid;
+    if(width<1200){
+        HeroVid=HeroMobVid;
+    }
     return(
         <Layout>
             <video className={classes.vid} width="1920" height="1080" autoplay="autoplay" loop muted="muted">
-                <source src={HeroVideo} type="video/mp4" media="screen and (min-width: 1200px)"/>
-                <source src={HeroMobVid} type="video/mp4" media="handheld and (min-width: 320px)"/>
+                <source src={HeroVid} type="video/mp4"/>
             </video>            
         </Layout>
     )
