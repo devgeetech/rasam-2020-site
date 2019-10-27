5fb65047663b145a9b54a10cb47af968ab6e351c
devgeetech <devgeetech@gmail.com>
2019-10-27 15:25:13 +0530

    new bg, new video source

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,15 @@ import Layout from '../components/hoc/Layout/Layout'
 
 import classes from '../styles/index.module.css'
 import HeroVideo from '../../content/vids/rasGli.mp4'
+import HeroMobVid from '../../content/vids/rasGliMob.mp4'
 
 
 const IndexPage = () => {
     return(
         <Layout>
             <video className={classes.vid} width="1920" height="1080" autoplay="autoplay" loop muted="muted">
-                <source src={HeroVideo} type="video/mp4" />
+                <source src={HeroVideo} type="video/mp4" media="(min-width: 1200px)"/>
+                <source src={HeroMobVid} type="video/mp4" media="(min-width: 320px)"/>
             </video>            
         </Layout>
     )
