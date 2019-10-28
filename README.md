8eb8d5f15ba4d9ddd53309fafb1a177208c33c06
devgeetech <devgeetech@gmail.com>
2019-10-28 16:43:38 +0530

    contact page update

--- README.md
+++ README.md
@@ -95,3 +95,11 @@ Looking for more guidance? Full documentation for Gatsby lives [on the website](
 [![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-hello-world)
 
 <!-- AUTO-GENERATED-CONTENT:END -->
+
+## Icon credits
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,60 @@
 import React from 'react'
 
+
 import Layout from '../components/hoc/Layout/Layout'
+import classes from '../styles/contact.module.css'
+
+
+import jpgBG from '../../content/images/jpg/vibe1.jpg'
+import locpin from '../../content/icons/locpinEd.svg'
+import mail from '../../content/icons/mailEd.svg'
+import fb from '../../content/icons/fbEd.svg'
+import insta from '../../content/icons/instaEd.svg'
+import twitter from '../../content/icons/twitterEd.svg'
+import youtube from '../../content/icons/youtubeEd.svg'
+import linkedin from '../../content/icons/linkedinEd.svg'
+
 
 const contact = () => {
     return(
         <Layout>
-            <h1>Contact</h1>
+            <img src={jpgBG} alt="Rasam BG" className={classes.BgIm}/>
+            <div className={classes.grad} />
+            <div className={classes.content}>
+                    <div className={classes.heading}>Contact Us</div>
+                    <div className={classes.thalluText}>
+                        <p className={classes.thalluMessage}>Hi! got questions? Shoot.</p>
+                        <div className={classes.contDets}>
+                            <div className={classes.contDet}>
+                                <img src={mail} alt="Mail icon" className={classes.svgIc}/>
+                                <a href="mailto:info@providence.edu.in">info@providence.edu.in</a>
+                            </div>
+                            <div className={classes.contDet}>
+                                <img src={locpin} alt="locpin" className={classes.svgIc}/>
+                                <a href="https://g.page/TheProvidenceCollege?share">Providence College of Engineering,
+                                 Angadical South, Chengannur</a>
+                            </div>
+                            <div className={classes.contDet}>
+                                <a href="http://facebook.com/theprovidencecollege">
+                                    <img src={fb} alt="locpin" className={classes.svgIc}/>
+                                </a>
+                                <a href="http://twitter.com/providencece">
+                                    <img src={twitter} alt="locpin" className={classes.svgIc}/>
+                                </a>
+                                <a href="http://instagram.com/providencece">
+                                    <img src={insta} alt="locpin" className={classes.svgIc}/>
+                                </a>
+                                <a href="http://prvdnc.com/youtube">
+                                    <img src={youtube} alt="locpin" className={classes.svgIc}/>
+                                </a>
+                                <a href="http://prvdnc.com/linkedin">
+                                    <img src={linkedin} alt="locpin" className={classes.svgIc}/>
+                                </a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
         </Layout>
 
     )
