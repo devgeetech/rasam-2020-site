adb5095cd8564dfa0a62eff49e852319f3e0b5a5
devgeetech <devgeetech@gmail.com>
2019-10-29 17:31:59 +0530

    cdn added 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import { Helmet } from "react-helmet"
 import Layout from '../components/hoc/Layout/Layout'
 
 import classes from '../styles/index.module.css'
-import HeroPCVid from '../../content/vids/rasGli.mp4'
-import HeroMobVid from '../../content/vids/rasGliMob.mp4'
+// import HeroPCVid from '../../content/vids/rasGli.mp4'
+// import HeroMobVid from '../../content/vids/rasGliMob.mp4'
 
 
 // let HeroVid = null;
@@ -64,10 +64,14 @@ const IndexPage = () => {
             </Helmet>
             <Layout>
                 <video className={classes.vid} width="1920" height="1080" autoPlay="autoPlay" loop muted="muted">
-                    <source src={HeroPCVid} type="video/mp4"/>
+                    {/* <source src={HeroPCVid} type="video/mp4"/> */}
+                    <source src="https://res.cloudinary.com/devgeetech/video/upload/ac_none,q_100/v1572348265/rasam/rasGli_uvmds2.webm" type="video/webm"/>
+                    <source src="https://res.cloudinary.com/devgeetech/video/upload/v1572348265/rasam/rasGli_uvmds2.mp4" type="video/mp4"/>
                 </video>
                 <video className={classes.mobVid} width="1920" height="1080" autoPlay="autoPlay" loop muted="muted">
-                    <source src={HeroMobVid} type="video/mp4"/>
+                    {/* <source src={HeroMobVid} type="video/mp4"/> */}
+                    <source src="https://res.cloudinary.com/devgeetech/video/upload/ac_none,q_100/v1572348264/rasam/rasGliMob_wcvila.webm" type="video/webm"/>
+                    <source src="https://res.cloudinary.com/devgeetech/video/upload/v1572348264/rasam/rasGliMob_wcvila.mp4" type="video/mp4"/>
                 </video>               
             </Layout>
         </React.Fragment>
